12c646c3f02391c911f86ae3ff2cd56ab0932259
Nurul Sundarani <sundarani@prisma.io>
2025-09-15 13:05:02 +0200

    (init) Initialise project

---
+++ src/caching.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
+
+async function main() {
+  const startTime = performance.now();
+
+  // Learn more about caching strategies:
+  // https://www.prisma.io/docs/accelerate/caching
+  const cachedUsersWithPosts = await prisma.user.findMany({
+    where: {
+      email: { contains: "alice" },
+    },
+    include: { posts: true },
+    cacheStrategy: {
+      swr: 30, // 30 seconds
+      ttl: 60, // 60 seconds
+    },
+  });
+
+  const endTime = performance.now();
+
+  // Calculate the elapsed time
+  const elapsedTime = endTime - startTime;
+
+  console.log(`The query took ${elapsedTime}ms.`);
+  console.log(`It returned the following data: \n`, cachedUsersWithPosts);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
